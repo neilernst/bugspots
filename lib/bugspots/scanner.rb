0fc157466645e6079e030cd699a5c1aa525dfc2c
Neil Ernst <neil@neilernst.net>
2013-09-06 12:22:43 -0400

    Add calculation of avg files per commit
    For a given period calculate how many files on average were affected
    per commit, for those files in the current treeish.

--- lib/bugspots/scanner.rb
+++ lib/bugspots/scanner.rb
@@ -2,7 +2,7 @@ require 'rainbow'
 require 'grit'
 
 module Bugspots
-  Fix = Struct.new(:message, :date, :files)
+  Fix = Struct.new(:message, :date, :files, :changes)
   Spot = Struct.new(:file, :score)
 
   def self.scan(repo, branch = "master", depth = 500, regex = nil, max_age = nil, min_age = nil)
@@ -16,25 +16,26 @@ module Bugspots
 
     tree = repo.tree(branch)
 
-    if max_age and !min_age  #kludge to account for passing of nil max_age resulting in git rev_list assuming it is 0
-          commit_list = repo.git.rev_list({:max_count => false, :no_merges => true, :pretty => "raw", :timeout => false, :max_age => max_age}, branch)
-    elif min_age and !max_age
-          commit_list = repo.git.rev_list({:max_count => false, :no_merges => true, :pretty => "raw", :timeout => false, :min_age =>  min_age}, branch)
-    elif min_age and max_age
-          commit_list = repo.git.rev_list({:max_count => false, :no_merges => true, :pretty => "raw", :timeout => false, :max_age => max_age, :min_age =>  min_age}, branch)
-    else #neither true
-      commit_list = repo.git.rev_list({:max_count => false, :no_merges => true, :pretty => "raw", :timeout => false}, branch)
-    end
+      commit_list = repo.git.rev_list({:max_count => false, :no_merges => true, :pretty => "raw", :timeout => false, :max_age => max_age, :min_age =>  min_age}, branch)
+
 
     Grit::Commit.list_from_string(repo, commit_list).each do |commit|
       if commit.message =~ regex
-        files = commit.stats.files.map {|s| s.first}.select{ |s| tree/s }
-        fixes << Fix.new(commit.short_message, commit.date, files)
+        files = commit.stats.files.map {|s| s.first}.select{ |s| tree/s } # don't include files not in that branch's current treeish (ie. they were deleted.)
+        fixes << Fix.new(commit.short_message, commit.date, files, files.length)
       end
     end
 
+    tot_change = 0
+    denom = 0
+    avg = nil
+
     hotspots = Hash.new(0)
     fixes.each do |fix|
+      if fix.changes > 0
+        tot_change += fix.changes
+        denom += 1
+      end
       fix.files.each do |file|
         if min_age  # support changing alg to use min_time, i.e. time of most recent specified commit
           t = 1 - ((Time.at(min_age.to_i) - fix.date).to_f / (Time.at(min_age.to_i) - fixes.last.date))
@@ -45,6 +46,10 @@ module Bugspots
       end
     end
 
+    if denom != 0
+      avg = tot_change.to_f / denom.to_f
+    end
+    puts "\t\tAvg # files per fix is ".foreground(:red) + avg.round(3).to_s
     spots = hotspots.sort_by {|k,v| v}.reverse.collect do |spot|
       Spot.new(spot.first, sprintf('%.4f', spot.last))
     end
